d5f31b3057d843fad8233594ce9a0dd2d415ff45
Folite25 <139177019+Folite25@users.noreply.github.com>
2024-10-02 18:07:21 +0200

    Update scripttest.lua

--- scripttest.lua
+++ scripttest.lua
@@ -1,1 +1,29 @@
-local 1 = 147
+if game.PlaceId -- 4483381587 then
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "Solaria Universal Script",
+   LoadingTitle = "Solaria Hub",
+   LoadingSubtitle = "by Folite25",
+   ConfigurationSaving = {
+      Enabled = false,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   },
+   Discord = {
+      Enabled = true,
+      Invite = "SCWK6xw2vN", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Solaria Universal Script",
+      Subtitle = "Key Sur Le Discord",
+      Note = "Nothing Here",
+      FileName = "SolariaHubKey", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"https://pastebin.com/raw/r3bDM1Wz"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
